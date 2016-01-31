30efed3b23e1b070c2be045e1e28e1d8f71348a9
BeVC <Bernard_Van_Couwenberghe@hotmail.com>
2016-01-31 20:51:52 +0100

    azure calls to db

--- FoodList/Components/FoodList/FoodListController.js
+++ FoodList/Components/FoodList/FoodListController.js
@@ -4,9 +4,9 @@
     angular.module("FoodList")
         .controller("FoodListController", FoodListController);
 
-    FoodListController.$inject = ["$mdDialog"];
+    FoodListController.$inject = ["$mdDialog", "Azureservice"];
 
-    function FoodListController($mdDialog) {
+    function FoodListController($mdDialog, Azureservice) {
         var vm = this;
         vm.foodStorageCol = [];
 
@@ -47,7 +47,15 @@
             })
                 .then(function (answer) {
                     var foodStorage = answer;
-                    vm.foodStorageCol.push(foodStorage);
+
+                    var foodStorageForBackend = FoodStorage.copyProperties(foodStorage, true);
+
+                Azureservice.insert('foodStorage', foodStorageForBackend)
+                    .then(function(result) {
+                        vm.foodStorageCol.unshift(result);
+                    }, function(err) {
+                        console.error("Azure error " + err);
+                    });
                 }, function () {
                     vm.status = "You cancelled the dialog";
                 });
@@ -55,11 +63,21 @@
 
         function init() { }
 
-        function removeFoodStorageItem(foodStorage,index, ev) {
-            vm.foodStorageCol.splice(index, 1);
+        function removeFoodStorageItem(foodStorage, index, ev) {
+            if (typeof foodStorage.id !== "undefined" || foodStorage.id != null)
+                Azureservice.update({
+                    id: foodStorage.id,
+                    present: false
+                }).then(function () {
+                    vm.foodStorageCol.splice(index, 1);
+                }, function (err) {
+                    console.error("Azure error " + err);
+                });
+            else
+                vm.foodStorageCol.splice(index, 1);
         }
 
-        function editFoodStorageItem(foodStorage,index, ev) {
+        function editFoodStorageItem(foodStorage, index, ev) {
             alert("edit");
         }
     }
